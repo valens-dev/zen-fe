1df2f118f87fa24f8e7f03d279ccc3186923326f
Ivo <ivo_gavran@hotmail.com>
2024-07-09 19:00:23 +0200

    chore: fix eslint issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+
 import { App } from './pages';
+
 import './styles/global.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
