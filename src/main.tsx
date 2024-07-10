2aab9c363358a6f50f5533eab8d109638e7d5f5c
Ivo Gavran <39406867+ivo-gavran@users.noreply.github.com>
2024-07-10 09:23:36 +0200

    Linting setup (#2)
    * chore: setup prettier
    
    * chore: setup eslint
    
    * chore: setup eslint
    
    * chore: removed dummy files
    
    * chore: fix eslint issues
    
    * chore: setup lint-staged
    
    * chore: add vscode ext recommendations and settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './pages/App'
-import './styles/global.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { App } from './pages';
+
+import './styles/global.css';
+
+createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+/**
+ * https://vitejs.dev/config/
+ */
+
+/* eslint-disable-next-line import/no-default-export */
 export default defineConfig({
   plugins: [react()],
-})
+});
