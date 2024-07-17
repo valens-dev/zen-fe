79bd90c778e1b1d4751bab08944154003fc761af
Edin Gondzo <79285646+edinatvalens@users.noreply.github.com>
2024-07-17 15:07:19 +0200

    feat: Add backend communication module (#12)
    * feat: Add backend communication module
    
    Co-authored-by: Ivo <ivo_gavran@hotmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { RootProvider } from '@/context/root-provider';
+
 import './styles/global.css';
 import { App } from './pages';
 
 createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
-    <App />
+    <RootProvider>
+      <App />
+    </RootProvider>
   </React.StrictMode>,
 );
--- src/types/material.ts
+++ src/types/material.ts
@@ -2,5 +2,32 @@ export enum MaterialType {
   Product = 'product',
   ManufacturingPart = 'manufacturing-part',
   PurchasingPart = 'purchasing-part',
-  Purchasing = 'Purchasing',
+}
+
+interface IAttribute {
+  name: string;
+  option: string;
+}
+
+interface IValue {
+  name: string;
+  value: number;
+  unit: string;
+  toleranceMin: number;
+  toleranceMax: number;
+}
+
+export interface IMaterial {
+  name: string;
+  type: string;
+  values: IValue[];
+  attributes: IAttribute[];
+  weight: number;
+  materialNumber: string;
+  netPrice: number;
+  VAT: number;
+  customsTarif: number;
+  description: string;
+  image: string;
+  packaging: boolean;
 }
