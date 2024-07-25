ceeca19dd3c37021de8ac220a2e97ad7b0b17da3
Edin Gondzo <79285646+edinatvalens@users.noreply.github.com>
2024-07-25 13:50:50 +0200

    fix: production api (#37)
    * fix: production api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios, { type AxiosInstance } from 'axios';
 
 export const api: AxiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.DEV ? '/api' : 'https://zen-admin.valens.dev/api',
 });
---
+++ src/services/material/utils.tsx
@@ -0,0 +1,40 @@
+import { type IProduct } from '@/components/material/material-table/types';
+
+import {
+  type IValue,
+  type IMaterial,
+  type IAttribute,
+  type MaterialType,
+} from '@/types/material';
+
+export function transformMaterialsToProducts(
+  materials?: IMaterial[],
+): IProduct[] {
+  if (!materials) {
+    return [];
+  }
+  return materials.map((material: IMaterial) => {
+    const heightValue = material.values.find((value: IValue) => {
+      return value.name === 'Height';
+    });
+    const heightFormatted = heightValue
+      ? `${heightValue.value} ${heightValue.unit}`
+      : '';
+
+    const materialValue =
+      material.attributes.find((attribute: IAttribute) => {
+        return attribute.name === 'material';
+      })?.option ?? '';
+
+    return {
+      imageUrl: material.image,
+      height: heightFormatted,
+      material: materialValue,
+      stock: 'In progress',
+      materialType: material.type as MaterialType,
+      name: material.name,
+      price: `${material.netPrice}$`,
+      weight: `${material.weight}kg`,
+    };
+  });
+}
