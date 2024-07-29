aad0e33c27618ef3d412ddb2e42be06da8c98874
edinatvalens <edin.gondzo@valens.dev>
2024-07-29 15:15:09 +0200

    feat: add index for station sequence

--- src/pages/station-page/constants.tsx
+++ src/pages/station-page/constants.tsx
@@ -4,7 +4,7 @@ import { Typography } from '@mui/material';
 
 import { StationOverview } from '@/components/station';
 import { stationData } from '@/components/station/constants';
-import { StationSequence } from '@/components/station/station-sequence/station-sequence';
+import { StationSequence } from '@/components/station/station-sequence';
 
 export function getStationTabs(): {
   label: string;
