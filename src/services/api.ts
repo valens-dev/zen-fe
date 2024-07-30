25c352c9517a09b70d851dc80afbdc5e411f66c2
edinatvalens <edin.gondzo@valens.dev>
2024-07-30 10:38:02 +0200

    chore: added env variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios, { type AxiosInstance } from 'axios';
 
+const baseURL = import.meta.env.VITE_API_URL as string;
+
 export const api: AxiosInstance = axios.create({
-  baseURL: 'https://zen-admin.valens.dev/api',
+  baseURL,
 });
