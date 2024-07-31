dd73b39a2a5ff1b224c84f927ee26809643cb3a0
edinatvalens <edin.gondzo@valens.dev>
2024-07-31 21:55:33 +0200

    feat: added station forward icon

--- src/shared/tabs-section/styles.ts
+++ src/shared/tabs-section/styles.ts
@@ -21,6 +21,9 @@ export const useStyles = tss.create(({ theme }) => {
       '&.Mui-selected': {
         color: theme.palette.light.tertiary,
       },
+      '& svg path': {
+        stroke: theme.palette.light.tertiary,
+      },
     },
     line: {
       width: '100%',
